ee375f5275262feca94074ab7efe0c1c507f5883
Gurpreet Singh <gurpreet@cse.iitb.ac.in>
2021-11-27 00:48:21 +0530

    communication

--- moodle/settings.py
+++ moodle/settings.py
@@ -138,3 +138,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'   #/dhvanit
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'moodlefloat@gmail.com'
+EMAIL_HOST_PASSWORD = 'sslproject'  #dhvanit/
